26ce44e1eed277aa8fc5b5d8c6cb46ef5d05a326
QuocViet20 <luongquocvietbk58@gmail.com>
2022-09-18 23:36:47 +0900

    update asm2

--- app.js
+++ app.js
@@ -114,7 +114,9 @@ mongoose
     //   }
     // });
     console.log("connected to mongodb!");
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("listening on *:3000");
+    });
   })
   .catch((err) => {
     console.log(err);
